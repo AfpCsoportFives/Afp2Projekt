bc3a0d6b2a20e37d7520d166f61d63c1ccb86bc1
ocbtomi36 <118051723+ocbtomi36@users.noreply.github.com>
2024-04-29 11:12:06 +0200

    Add start page to the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+        <header className="hero">
+            <div id="navbar" className="navbar">
+                <h1 className="logo">
+                <span className="text-primary"> <i className="fa-brands fa-react"></i> Rendezvényszervező</span>
+                </h1>
+            </div>
+            <div className="content">
+                <h1>Fogaljon weboldalunkon keresztül</h1>
+                <p>Nálunk biztos van olyan rendezvény ami érdekelheti önt!</p>
+                <a href="login.html" className="btn"><i className="fas fa-chevron-right"></i> Bejelentkezés</a>
+                <h4>Nincs még felhasználói fiókja?</h4>
+                <a href="registration.html" className="btn-little">Regisztráció</a>
+            </div>
+        </header>
+        <main>
+            <section id="rolunk" className="icons bg-dark">
+                <div className="flex-items">
+                    <div>
+                        <i className="fas fa-university fa-2x"></i>
+                        <div>
+                            <h3>Rendezvény regisztrálás</h3>
+                            <p>Lorem ipsum dolor, sit amet
+                            consectetur adipisicing elit. 
+                            Tempore, repellendus?</p>
+                        </div>
+                    </div>
+                    <div>
+                        <i className="fa-solid fa-ticket fa-2x"></i>
+                        <div>
+                            <h3>Helyfoglalás</h3>
+                            <p>Lorem ipsum dolor, sit amet
+                            consectetur adipisicing elit. 
+                            Tempore, repellendus?</p>
+                        </div>
+                    </div>
+                    <div>
+                        <i className="fa-solid fa-user fa-2x"></i>
+                        <div>
+                            <h3>Tanácsadás</h3>
+                            <p>Lorem ipsum dolor, sit amet
+                            consectetur adipisicing elit. 
+                            Tempore, repellendus?</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer className="footer bg-light">
+            <div className="social">
+                <a href="#"><i className="fab fa-facebook fa-2x"></i></a>
+                <a href="#"><i className="fab fa-twitter fa-2x"></i></a>
+                <a href="#"><i className="fab fa-youtube fa-2x"></i></a>
+                <a href="#"><i className="fab fa-linkedin fa-2x"></i></a>
+            </div>
+            <p>Copyright &copy; 2024</p>
+        </footer>
+        </>
+    )
 }
 
-export default App;
+export default App
