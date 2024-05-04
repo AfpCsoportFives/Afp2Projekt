8ae8fe8d6cf2d0f7e99100cad320dbe03a1bbad4
Kjanos141 <Kjanos141@gmail.com>
2024-05-04 14:51:20 +0200

    server.js válaszok javítása, registration,listEvent,createEvent klétrehozása
    server.js
    - válaszok javítása
    - registration,listEvent,createEvent klétrehozása alapértelmezett válaszokkal.

--- server/server.js
+++ server/server.js
@@ -16,8 +16,8 @@ app.post("/login",async (req,res)=>{
     /*const {email, password}=req.body;
     const user = new User();
     const loginRes=await user.login(email,password);
-    if(loginRes.success) res.json(loginRes.response);*/
-    res.json({cookie:"asdjkhbashdjkaskdjm",logintSuccess:true});
+    if(loginRes.success) res.json({cookie:loginRes.response.cookie,success:loginRes.success});*/
+    res.json({cookie:"asdjkhbashdjkaskdjm",success:true});
 })
 
 //kijelentkezés
@@ -25,28 +25,38 @@ app.post("/logout",async (req,res)=>{
     /*const {userId}=req.body;
     const user = new User(userId);
     const logoutRes=await user.logout();
-    if(logoutRes.success) res.json(logoutRes.response)*/
-    res.json({logoutSuccess:true})
+    if(logoutRes.success) res.json({cookie:logoutRes.response.cookie,success:logoutRes.success});*/
+    res.json({success:true})
 })
 
 //Regisztráció
-app.post("/registration",(req,res)=>{
-    
+app.post("/registration",async (req,res)=>{
+    /*const {email,password,firstname,lastname}=req.body;
+    const user = new User();
+    const regRes=await user.registartion(email,password,firstname,lastname);
+    if(regRes.success) res.json({cookie:regRes.response.cookie,success:regRes.success});*/
+    res.json({cookie:"asdjkhbashdjkaskdjm",success:true});
 })
 
-//Meghívó küldése
-app.post("/sendInv",(req,res)=>{
-    
-})
-
-//Rendezvényekl listázása
-app.get("/listEvents",(req,res)=>{
-    
+//Rendezvények listázása
+app.get("/listEvents",async (req,res)=>{
+    /*const getAllEventRes=await Event.getAllEvent()
+    if(getAllEventRes.success) res.json({success:getAllEventRes.success,eventList:getAllEventRes.response});*/
+    res.json({success:true,eventList:[
+        {id:1,name:"Rendezvény 1",date:"2024.05.04 12:00:00",description:"Rendezvény1 leírás"},
+        {id:1,name:"Rendezvény 1",date:"2024.05.04 12:00:00",description:"Rendezvény1 leírás"},
+        {id:1,name:"Rendezvény 1",date:"2024.05.04 12:00:00",description:"Rendezvény1 leírás"},
+        {id:1,name:"Rendezvény 1",date:"2024.05.04 12:00:00",description:"Rendezvény1 leírás"},
+    ]});
 })
 
 //Rendezvény létrehozása
-app.post("/createEvent",(req,res)=>{
-    
+app.post("/createEvent",async (req,res)=>{
+    /*const {name,date,description,userId}=req.body;
+    const createEventRes=await Event.createEvent(name,date,description,userId);
+    if(createEventRes.success) res.json({eventId:createEventRes.eventId,success:createEventRes.success});*/
+    res.json({eventId:10,success:createEventRes.success});
+
 })
 
 //Rendezvény frissítése
