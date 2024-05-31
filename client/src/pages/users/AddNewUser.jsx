74c89e24a617218adfd6391a899fabe2af3a0dbc
ocbtomi36 <ocbtomi36@outlook.hu>
2024-05-31 17:04:57 +0200

    Api in AddNewUser is finished

--- client/src/pages/users/AddNewUser.jsx
+++ client/src/pages/users/AddNewUser.jsx
@@ -1,55 +1,143 @@
 import React from 'react'
 
 function AddNewUser() {
+
+    const [formData, setFormData] = useState({
+        felhasznalokId:'',	
+        Vezeteknev: '', 	
+        Keresztnev: '',	
+        Felhasznalonev: '',	
+        Jelszo: '',	
+        Email: '',
+        SzuletesiDatum: '',
+        Neme: '', 	
+        Iranyitoszam: '', 	
+        Varos: '', 	
+        UtcaHazszam:'',
+        Foglalkozasa:'',	
+        IskolaiVegzettsege:'',	
+        RegisztracioDatuma:'',
+        FelhasznaloStatusza:'',	
+        Cookie: '',	
+        CookieExpire: ''
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value
+        });
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Konvertáljuk a SzabadHelyekSzama értékét numerikussá, ha lehetséges
+        const updatedFormData = {
+          ...formData
+        };
+    
+        try {
+          const response = await fetch('http://localhost:5000/createuser', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedFormData)
+          });
+          if (response.ok) {
+            alert('A felhasználó sikeresen hozzáadva!');
+            setFormData({
+                felhasznalokId: null,
+                Vezeteknev: '',
+                Keresztnev: '',
+                FelhasznaloNev: '',
+                Jelszo: '',
+                Email: '',
+                SzuletesiDatum: '',
+                Neme: '',
+                Iranyitoszam: '',
+                Varos: '',
+                UtcaHazszam: '',
+                Foglalkozasa: '',
+                IskolaiVegzettsege: '',
+                RegisztracioDatuma: '',
+                FelhasznaloStatusza: '',
+                Cookie: null,
+                CookieExpire: null
+              });
+              
+          } else {
+            alert('Hiba történt a felhasználó hozzáadása során.');
+          }
+        } catch (error) {
+          console.error('Hiba történt:', error);
+          alert('Hiba történt a felhasználó hozzáadása során.');
+        }
+      };
+
+
+
   return (
-    <div id='registration'> 
-        <div className="reg-wrapper">
-            <form action="#" method="post">
-                <h1>Regisztráció</h1>
-                <p>Kérjük adja meg az adatait</p>
-                <div className="input-box">
-                    <input type="text" id="username" name="username" placeholder="Felhasználónév"/>
-                    <i className="fa-solid fa-user"></i>
-                </div>
-                <div className="input-box">
-                    <input type="password" id="password" name="password" placeholder="Jelszó" />
-                    <i className="fa-solid fa-lock"></i>
-                </div>
-                <div class="input-box">
-                    <input type="email" id="email" name="email" placeholder="E-mail" />
-                    <i className="fa-solid fa-envelope"></i>
-                </div>
-                <div className="input-box">
-                    <input type="text" id="habitation" name="habitation" placeholder="Lakóhely" />
-                    <i className="fa-solid fa-home"></i>
-                </div>
-                <div className="input-box-dropdown">
-                    <select name="gender">
-                        <option value="" selected disabled hidden>Válasszon nemet</option>
-                        <option value="male">Férfi</option>
-                        <option value="female">Nő</option>
-                        <option value="other">Egyéb</option>
-                    </select>
-                    <i className="fa-solid fa-chevron-down"></i>
-                </div>
-                <div className="input-box">
-                    <input type="text" id="job" name="job" placeholder="Foglalkozás" />
-                    <i className="fa-solid fa-briefcase"></i>
-                </div>
-                <div className="input-box">
-                    <input type="text" id="scoolar" name="scoolar" placeholder="Iskolai végzettség" />
-                    <i className="fa-solid fa-graduation-cap"></i>
-                </div>
-                <h5>Születési dátum</h5>
-                <div className="input-box-date">
-                    <input type="date" id="dateOfBirth" name="dateOfBirth" placeholder="Születési dátum" />
-                    <i className="fa-solid fa-calendar-alt"></i>
-                </div>
-                <br />
-                <button type="submit" className="btn-reg">Elküld</button>
-            </form>
-        </div>
+    <div id='registration'>
+    <div className="reg-wrapper">
+        <form onSubmit={handleSubmit}>
+            <h1>Regisztráció</h1>
+            <p>Kérjük adja meg az adatait</p>
+            <div className="input-box">
+                <input type="text" id="Vezeteknev" name="Vezeteknev" placeholder="Vezetéknév" required/>
+                <i className="fa-solid fa-user"></i>
+            </div>
+            <div className="input-box">
+                <input type="text" id="Keresztnev" name="Keresztnev" placeholder="Keresztnév" required/>
+                <i className="fa-solid fa-user"></i>
+            </div>
+            <div className="input-box">
+                <input type="text" id="Neme" name="Neme" placeholder="Nem" required/>
+                <i className="fa-solid fa-venus-mars"></i>
+            </div>
+            <div className="input-box">
+                <input type="date" id="SzuletesiDatum" name="SzuletesiDatum" placeholder="Születési Dátum" required/>
+                <i className="fa-solid fa-calendar-alt"></i>
+            </div>
+            <div className="input-box">
+                <input type="text" id="IskolaiVegzettsege" name="IskolaiVegzettsege" placeholder="Iskolai végzettség" required/>
+                <i className="fa-solid fa-graduation-cap"></i>
+            </div>
+            <div className="input-box">
+                <input type="text" id="Foglalkozasa" name="Foglalkozasa" placeholder="Foglalkozás" required/>
+                <i className="fa-solid fa-briefcase"></i>
+            </div>
+            <div className="input-box">
+                <input type="text" id="Varos" name="Varos" placeholder="Város" required/>
+                <i className="fa-solid fa-city"></i>
+            </div>
+            <div className="input-box">
+                <input type="text" id="Iranyitoszam" name="Iranyitoszam" placeholder="Irányítószám" required/>
+                <i className="fa-solid fa-map-marker-alt"></i>
+            </div>
+            <div className="input-box">
+                <input type="text" id="UtcaHazszam" name="UtcaHazszam" placeholder="Utca, Házszám" required/>
+                <i className="fa-solid fa-road"></i>
+            </div>
+            <div className="input-box">
+                <input type="text" id="FelhasznaloNev" name="FelhasznaloNev" placeholder="Felhasználónév" required/>
+                <i className="fa-solid fa-user"></i>
+            </div>
+            <div className="input-box">
+                <input type="password" id="Jelszo" name="Jelszo" placeholder="Jelszó" required/>
+                <i className="fa-solid fa-lock"></i>
+            </div>
+            <div className="input-box">
+                <input type="email" id="Email" name="Email" placeholder="E-mail" required/>
+                <i className="fa-solid fa-envelope"></i>
+            </div>
+            <br />
+            <button type="submit" className="btn-reg">Elküld</button>
+        </form>
     </div>
+</div>
+
   )
 }
 
